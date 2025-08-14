df0736c1e9f84197a6c10d4a862a94b9bea949ce
Bruno Morales <bruno.morales.ia2016@gmail.com>
2025-08-14 08:54:27 -0300

    Update validacion.js

--- registro/js/validacion.js
+++ registro/js/validacion.js
@@ -14,9 +14,11 @@ document.getElementById("regBtn").addEventListener("click", function () {
     let email = document.getElementById("email").value.trim();
     let password1 = document.getElementById("password1").value.trim();
     let password2 = document.getElementById("password2").value.trim();
+    let terminos = document.getElementById("terminos").checked;
 
-    if (nombre && apellido && email && password1 && password2 && password1 !== "" && password1 === password2) {
+    if (nombre && apellido && email && password1 && password2 && terminos && password1 !== "" && password1 === password2) {
         showAlertSuccess();
     } else {
         showAlertError();
     }
+
