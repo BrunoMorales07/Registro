eaabb5ac76cb30b871a2c8f273efae5c7864f1b3
boris <bssokolovs@gmail.com>
2025-08-14 08:33:00 -0300

    Update validacion.js

--- registro/js/validacion.js
+++ registro/js/validacion.js
@@ -5,3 +5,15 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+
+document.getElementById("regBtn").addEventListener("click", function () {
+    const password1 = document.getElementById("password1").value;
+    const password2 = document.getElementById("password2").value;
+
+    
+    if (password1 !== "" && password1 === password2) {
+        showAlertSuccess();
+    } else {
+        showAlertError();
+    }
+});
