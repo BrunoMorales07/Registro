8a482dfd6a431b7beef7da3a2ec6989d234e1996
BrunoMorales07 <bruno.morales.ia2016@gmail.com>
2025-08-14 08:41:33 -0300

    Update validacion.js

--- registro/js/validacion.js
+++ registro/js/validacion.js
@@ -5,3 +5,17 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+
+document.getElementById("regBtn").addEventListener("click", function () {
+    let nombre = document.getElementById("nombre").value.trim();
+    let apellido = document.getElementById("apellido").value.trim();
+    let email = document.getElementById("email").value.trim();
+    let password1 = document.getElementById("password1").value.trim();
+    let password2 = document.getElementById("password2").value.trim();
+
+    if (nombre && apellido && email && password1 && password2) {
+        showAlertSuccess();
+    } else {
+        showAlertError();
+    }
+});
